91eeeb5f8b72f04cedcd042db7f7ba4f3f0c7baa
Kapil Singh <kapex.coder@gmail.com>
2025-08-18 02:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
  function calculateArea() {
-            // Get the radius from the user using the prompt() function
-            let radius = prompt("Enter the radius of the circle:");
-            // write you code here and display the result to the user
+	// Get the radius from the user using the prompt() function
+	let radius = prompt("Enter the radius of the circle:");
+	// write you code here and display the result to the user
+
+	 const area = (3.1419 * (radius ** 2)).toFixed(2);
+
+	 console.log(`The area of the circle with radius ${radius} is ${area}`)
            
 }
-calculateArea();
+// calculateArea();
